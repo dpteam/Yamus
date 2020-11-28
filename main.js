b18db96c0ef80b993e76ae1efbfe4a47ffae775f
DartPower <>
2020-11-29 00:56:26 +0300

    Fix

--- main.js
+++ main.js
@@ -88,3 +88,4 @@ app.on('activate', function() {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+app.on('uncaughtException', (err) => { console.error('uncaughtException', err); });
